6db35c9baba27d0f784cf590528abc915441a7da
akucoba231 <aku.coba231@gmail.com>
2026-06-12 00:52:35 +0700

    Add files via upload

---
+++ dbKey.js
@@ -0,0 +1,5 @@
+let KEY_SISWA = "akun_siswa"; // dataSiswa
+let KEY_GURU = "akun_guru"; // dataAkunGuru
+let KEY_ASSESMENT = "data_assesment"; // dataAssesment
+let KEY_RIWAYAT = "data_riwayat"; // riwayatPenilaian
+let KEY_KRITERIA = "data_kriteria"; // kriteriaAssesment
